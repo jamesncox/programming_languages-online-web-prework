0516ab7d86a5a3c7781225ef8db45f9e1ccd77d6
James Cox <jamesncox@yahoo.com>
2019-03-15 16:06:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,8 +4,10 @@ def reformat_languages(languages)
  new_hash = {}
  languages.each do |style, data|
    data.each do |language, type|
-    
-     binding.pry
+    if !new_hash[language]
+     new_hash[language] = {}
    end
+   binding.pry
+  end
  end
 end
